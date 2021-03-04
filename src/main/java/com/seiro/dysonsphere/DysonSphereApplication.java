53cf3539c3e7416e49b639b60e32864485266815
seiro <seiroka@outlook.com>
2021-03-04 22:59:47 +0800

    dyson assist backend

---
+++ src/main/java/com/seiro/dysonsphere/DysonSphereApplication.java
@@ -0,0 +1,13 @@
+package com.seiro.dysonsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DysonSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DysonSphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seiro/dysonsphere/DysonSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seiro.dysonsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DysonSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
